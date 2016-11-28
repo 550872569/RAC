f58e1fe4623d778b797d952642bea71cf3ad5283
YY <550872569@qq.com>
2016-11-29 00:49:46 +0800

    COMMIIT INIT

---
+++ RACCode/RACCode/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  RACCode
+//
+//  Created by Yan on 16/11/29.
+//  Copyright © 2016年 YY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
